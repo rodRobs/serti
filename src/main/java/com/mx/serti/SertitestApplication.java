047885912c8c823217ff59172ba46165e7f99ade
Rodrigo Robles <rodrigorobles@192.168.1.15>
2024-03-20 13:47:07 -0600

    Initial commit

---
+++ src/main/java/com/mx/serti/SertitestApplication.java
@@ -0,0 +1,13 @@
+package com.mx.serti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertitestApplication.class, args);
+	}
+
+}
