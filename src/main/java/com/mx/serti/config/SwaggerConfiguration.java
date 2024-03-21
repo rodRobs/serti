1242a9ac168f87a2d2c3dd8b30004ca62f7eeba7
Rodrigo Robles <ro_bless_skate@hotmail.com>
2024-03-21 08:19:39 -0600

    Configuración para swagger

---
+++ src/main/java/com/mx/serti/config/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.mx.serti.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        String[] paths = {
+                "/*"
+        };
+        return GroupedOpenApi.builder().group("serti").pathsToMatch(paths).build();
+    }
+
+}
